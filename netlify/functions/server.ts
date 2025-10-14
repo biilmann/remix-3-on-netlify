2d46464f6a488fde30460c39e38e24de0460f2bb
Philippe Serhal <philippe.serhal@gmail.com>
2025-10-14 17:28:52 -0400

    chore: configure Netlify deploy

---
+++ netlify/functions/server.ts
@@ -0,0 +1,16 @@
+import type { Context } from "@netlify/functions";
+
+import { router } from "../../app/router.ts";
+
+export default async (req: Request, _context: Context) => {
+  try {
+    return await router.fetch(req);
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
+
+export const config = {
+  path: "/*",
+};
